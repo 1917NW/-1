7c03a7c7ccedc6370b341cf5857a9f90f9f3b7cb
1917NW <3177430867@qq.com>
2022-03-12 10:44:56 +0800

    new name

--- lab01/Cache优化/优化算法arm.cpp
+++ lab01/Cache优化/优化算法arm.cpp
@@ -4,11 +4,14 @@
 using namespace std;
 int main()
 {
-	
+	int count=20;
+	double time=0;
 	const int n = 600;
-	int b[n][n];
+	int** b=new int*[n];
+	for(int i=0;i<n;i++)
+		b[i]=new int[n];
 	int sum[n]={0};
-	int a[n];
+	int* a=new int[n];
 	for(int i=0;i<n;i++)
     {
          a[i]=1;
@@ -19,7 +22,7 @@ int main()
 		b[i][j]=i+j;
 	}
 	struct timeval t_start,t_end;
-
+	for(int i=count;i>0;i--){
 	gettimeofday(&t_start,NULL);
     for(int i=0;i<n;i++)
     {
@@ -27,5 +30,7 @@ int main()
             sum[j]+=a[i]*b[i][j];
     }
 	gettimeofday(&t_end,NULL);
-    cout<<"Cost: "<<(t_end.tv_usec-t_start.tv_usec)/1000<<"ms"<<endl;
+	time+=(t_end.tv_usec-t_start.tv_usec);
+	}
+    cout<<"Cost: "<<time/(count*1000)<<"ms"<<endl;
 }
